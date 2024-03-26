af7eb7024e66795a7420cf2a2c6aec57a04ad15a
Oliver Atkinson <>
2024-03-26 15:13:10 -0600

    gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://rushmore75.github.io',
+    base: 'offbrand-demo',
+});
